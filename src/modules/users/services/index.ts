6294ed3bcb707f0e863a922bfd6bcaec3ab55e86
Cleber Gomes <fclebinho@gmail.com>
2020-05-18 01:54:58 -0300

    Atualização do perfil

--- src/modules/users/services/index.ts
+++ src/modules/users/services/index.ts
@@ -1,5 +1,6 @@
 export { default as SessionCreateService } from '@modules/users/services/session-create';
 export { default as UserCreateService } from '@modules/users/services/user-create';
 export { default as UserAvatarUpdateService } from '@modules/users/services/user-avatar-update';
+export { default as UserProfileUpdateService } from '@modules/users/services/user-profile-update';
 export { default as RetrievePasswordService } from '@modules/users/services/retrieve-password';
 export { default as ResetPasswordService } from '@modules/users/services/reset-password';
